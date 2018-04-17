f67819f31c69ac9c03fe49b95d92fd5cf75a7b85
KByron1 <37088682+KByron1@users.noreply.github.com>
2018-04-17 16:24:11 -0400

    Changed color gradient

--- heatmap.js
+++ heatmap.js
@@ -23,7 +23,7 @@
 var HeatmapConfig = {
   defaultRadius: 40,
   defaultRenderer: 'canvas2d',
-  defaultGradient: { 0.25: "rgb(0,0,255)", 0.55: "rgb(0,255,0)", 0.85: "yellow", 1.0: "rgb(255,0,0)"},
+  defaultGradient: { -2.0: "rgb(0,0,255)", -1.50: "rgb(255,128,0)", -1.0: "rgb(255, 255, 0)", 0.99: "rgb(255, 255, 255)", 1.5: "rgb(128, 255, 0)", 1.99: "rgb(0,255,0)"},
   defaultMaxOpacity: 1,
   defaultMinOpacity: 0,
   defaultBlur: .85,
